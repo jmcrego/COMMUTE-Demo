16e27bac470d63e696c78f7fb523994b271232eb
Josep Crego <josep.crego@systrangroup.com>
2024-01-18 12:35:55 +0100

    socket

--- scripts/server.py
+++ scripts/server.py
@@ -1,107 +1,110 @@
 import sys
-import logging
-import pyonmttok
 import ctranslate2
 from faster_whisper import WhisperModel
-import numpy as np
-from pydub import AudioSegment
-from flask import Flask, request, jsonify
-from flask_cors import CORS  # Import the CORS module
-
-from pydub import AudioSegment
-from io import BytesIO
-
+import pyonmttok
+import time
+import logging
 logging.basicConfig(format='[%(asctime)s.%(msecs)03d] %(levelname)s %(message)s', datefmt='%Y-%m-%d_%H:%M:%S', level=getattr(logging, 'INFO', None), filename=None)
 
 device = 'cpu' #cpu or cuda
-HOST = '0.0.0.0'
-PORT = 12345
-
 model_size = 'tiny'
 Transcriber = WhisperModel(model_size_or_path=model_size, device=device, compute_type='int8')
-
-ct2 = '/nfs/RESEARCH/crego/projects/COMMUTE-Demo/checkpoints/checkpoint-50000.pt.ct2'
+ct2 = '/Users/crego/Desktop/COMMUTE-Demo/scripts/model/checkpoint-50000.pt.ct2'
 Translator = ctranslate2.Translator(ct2, device=device)
-
-bpe = '/nfs/RESEARCH/crego/projects/COMMUTE-Demo/config/bpe-ar-en-fr-50k'
+bpe = '/Users/crego/Desktop/COMMUTE-Demo/scripts/model/bpe-ar-en-fr-50k'
 Tokenizer = pyonmttok.Tokenizer("aggressive", joiner_annotate=True, preserve_placeholders=True, bpe_model_path=bpe)
 
+import asyncio
+import websockets
+import base64
+import pyaudio
+from pydub import AudioSegment
+import io
+import json
 
-def convert_webm_to_wav(webm_blob):
-    # Convert WebM blob to AudioSegment
-    audio_segment = AudioSegment.from_file(BytesIO(webm_blob), format="webm")
-    # Convert AudioSegment to WAV blob
-    wav_blob = BytesIO()
-    audio_segment.export(wav_blob, format="wav")
-    return wav_blob    
+ending_suffixes = ['.', '?', '!']
+delay_sec = 0.1  # Adjust this value based on your requirements
+sample_rate = 44100
+bytes_per_sample = 2  # Assuming 16-bit PCM, adjust if different
+distance_to_end = 1
+save_audio_sentences = False
 
-def describe(audio_blob):
-    # running this function changes the audio_blob and inutilizes it
-    # Process the audio Blob with pydub
-    audio_stream = BytesIO(audio_blob.read())
-    audio_segment = AudioSegment.from_file(audio_stream)
-    logging.info('Channels: {}'.format(audio_segment.channels))
-    logging.info('Frame Rate: {} Hz'.format(audio_segment.frame_rate))
-    logging.info('Sample Width: {} bits/sample'.format(audio_segment.sample_width))
-    logging.info('Duration: {} sec'.format(len(audio_segment) / 1000.0))
-
-def transcribe(audio_blob, lang_src, beam_size=5, history=None, task='transcribe'):
+def transcribe(wav_data, lang_src, beam_size=5, history=None, task='transcribe'):
+    #blob_data = blob.Blob(wav_data)#, type='audio/wav')
     language = None if lang_src == 'pr' else lang_src
-    segments, info = Transcriber.transcribe(audio_blob, language=language, task=task, beam_size=beam_size, vad_filter=True, word_timestamps=True, initial_prompt=history)
+    segments, info = Transcriber.transcribe(io.BytesIO(wav_data), language=language, task=task, beam_size=beam_size, vad_filter=True, word_timestamps=True, initial_prompt=history)
+    ending_time = 0
+    eos = False
     transcription = []
     for segment in segments:
-        for word in segment.words:
+        for i,word in enumerate(segment.words):
             transcription.append(word.word)
             logging.info("word\t{}\t{}\t{}".format(word.start,word.end,word.word))
-    return ''.join(transcription).strip(), info.language
+            if i < len(segment.words)-distance_to_end and any(word.word.endswith(suffix) for suffix in ending_suffixes):
+                eos = True
+                ending_time = word.end
+    lang_src = info.language
+    transcription = ''.join(transcription).strip()
+    logging.info('lang_src = {}, eos = {}, transcription = {} ending_time = {}'.format(lang_src, eos, transcription, ending_time))
+    return transcription, eos, lang_src, ending_time*1000
 
 def translate(transcription, lang_tgt):
-    if lang_tgt == '':
+    if lang_tgt == '' or transcription == '':
         return ''
     translation = ''
     input_stream = "｟" + lang_tgt + "｠" + " " + transcription
     results = Translator.translate_batch([Tokenizer(input_stream)])
     translation = Tokenizer.detokenize(results[0].hypotheses[0])
+    logging.info('translation = {}'.format(translation))
     return translation
 
-def processRequest(input_data):
-    audio_blob = request.files['audio']
-    audio_blob = convert_webm_to_wav(audio_blob)
-    lang_src = request.form.get('lang_src')
-    lang_tgt = request.form.get('lang_tgt')
-    firstChunk = int(request.form.get('firstChunk'))
-    nChunks = int(request.form.get('nChunks'))
-    logging.info('lang_src: {}, lang_tgt: {}, firstChunk: {}, nChunks: {}'.format(lang_src, lang_tgt, firstChunk, nChunks))
-    #describe(audio_blob)
-    transcription, lang_src = transcribe(audio_blob, lang_src)
-    logging.info('transcription = {}'.format([lang_src, transcription]))
-    translation = translate(transcription, lang_tgt)
-    logging.info('translation = {}'.format(translation))
-    output_data = {
-        "lang_src": lang_src,
-        "lang_tgt": lang_tgt,
-        "transcription": transcription,
-        "translation": translation,
-        "firstChunk": firstChunk + nChunks, 
-        "status": "success",
-        "message": "Request processed successfully"
-    }
-    return output_data
+async def handle_connection(websocket, path):
+    p = pyaudio.PyAudio()
+    prefix_wav = b""
 
-app = Flask(__name__)
-CORS(app, supports_credentials=True)  # Enable CORS for all routes
-@app.route('/pipeline_asr_mt', methods=['POST'])
-def pipeline_asr_mt():
-    #input_data = request.get_json()
-    #logging.info('Server: request')
-    output_data = processRequest(request) #input_data)
-    logging.info('Server: response output_data={}'.format(output_data))
-    return jsonify(output_data)
+    try:
+        while True:
 
-if __name__ == '__main__':
-    app.run(host=HOST, port=PORT, debug=True)
+            request_json = await websocket.recv()
+            curr_time = time.strftime("%Y-%m-%d_%H:%M:%S")
+            request = json.loads(request_json)
+            audio_data_base64 = request.get('audioData', '')
+            nChunk = request.get('nChunk', '')
+            lang_src = request.get('lang_src', '')
+            lang_tgt = request.get('lang_tgt', '')
+            logging.info('SERVER: Received nChunk={} lang_src={} lang_tgt={}'.format(nChunk, lang_src, lang_tgt))
+            ### compose new audio with prefix
+            audio_chunk = base64.b64decode(audio_data_base64)
+            prefix_wav += audio_chunk
+            ### transcript and translate
+            transcription, eos, lang_src, ending_time_ms = transcribe(prefix_wav, lang_src)
+            translation = translate(transcription, lang_tgt)
+            response_dict = {'transcription': transcription, 'translation': translation, 'eos': eos, 'lang_src': lang_src}
+            logging.info('SERVER: Send={}'.format(response_dict))
+            ### update prefix save sentence
+            if eos:
+                desired_bytes = int((ending_time_ms / 1000) * sample_rate * bytes_per_sample)
+                if save_audio_sentences:
+                    file_path = 'audio_{}_{}.wav'.format(nChunk,curr_time)
+                    with open(file_path, 'wb') as file:
+                        file.write(prefix_wav[:desired_bytes])
+                if desired_bytes >= len(prefix_wav):
+                    prefix_wav = b""  # empty prefix audio
+                else:
+                    prefix_wav = prefix_wav[desired_bytes:]
 
+            await websocket.send(json.dumps(response_dict))
+            await asyncio.sleep(delay_sec)
 
+    except websockets.ConnectionClosed:
+        print("Connection closed.")
+    finally:
+        p.terminate()
+
+start_server = websockets.serve(handle_connection, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
 
 
 
